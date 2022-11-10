9344648325b79810f35d7a3c0e395f8444f901a7
Andre da Silva <apdasil@emory.edu>
2022-11-10 08:33:21 -0500

    initial commit-- following along with Dave Gray's tutorial on CSS found at https://www.youtube.com/watch?v=n4R2E7O-Ngo

---
+++ src/main/java/com/example/learningcss/LearningCssApplication.java
@@ -0,0 +1,13 @@
+package com.example.learningcss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningCssApplication {
+
+     public static void main(String[] args) {
+          SpringApplication.run(LearningCssApplication.class, args);
+     }
+
+}
---
+++ src/test/java/com/example/learningcss/LearningCssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learningcss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningCssApplicationTests {
+
+     @Test
+     void contextLoads() {
+     }
+
+}
